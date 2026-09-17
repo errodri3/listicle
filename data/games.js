24e3e8e4b8ba8b25a3d549b7a69eb81e9924686b
Evelyn Rodriguez <errodriguez0703@gmail.com>
2026-09-17 04:23:33 -0700

    add games data file

---
+++ data/games.js
@@ -0,0 +1,68 @@
+const games = [
+  {
+    id: "magic-the-gathering",
+    name: "Magic: The Gathering",
+    image: "/images/magic-the-gathering.jpg",
+    description: "Trading card game where players cast spells and summon creatures to defeat their opponent.",
+    category: "Trading Card Game",
+    players: "2+"
+  },
+  {
+    id: "lorcana",
+    name: "Disney Lorcana",
+    image: "/images/lorcana.jpg",
+    description: "Trading card game where players use Disney characters as 'glimmers' to battle for control of the board.",
+    category: "Trading Card Game",
+    players: "2"
+  },
+  {
+    id: "star-wars",
+    name: "Star Wars: Unlimited",
+    image: "/images/star-wars.jpg",
+    description: "Trading card game set in the Star Wars universe where players build decks to defeat their opponent.",
+    category: "Trading Card Game",
+    players: "2"
+  },
+  {
+    id: "riftbound",
+    name: "Riftbound",
+    image: "/images/riftbound.jpg",
+    description: "Trading card game based on the League of Legends universe.",
+    category: "Trading Card Game",
+    players: "2"
+  },
+  {
+    id: "animal-crossing-monopoly",
+    name: "Animal Crossing Monopoly",
+    image: "/images/animal-crossing-monopoly.jpg",
+    description: "Animal Crossing-themed version of Monopoly where you buy, sell, and trade to build up your island.",
+    category: "Board Game",
+    players: "2-6"
+  },
+  {
+    id: "crabs-in-a-bucket",
+    name: "Crabs in a Bucket",
+    image: "/images/crabs-in-a-bucket.jpg",
+    description: "Party card game about pulling other players' cards to keep yourself in the bucket the longest.",
+    category: "Party Game",
+    players: "3-6"
+  },
+  {
+    id: "rainbow-shrimp",
+    name: "Rainbow Shrimp",
+    image: "/images/rainbow-shrimp.jpg",
+    description: "Fast-paced card matching game where players race to slap the matching card.",
+    category: "Party Game",
+    players: "2-6"
+  },
+  {
+    id: "uno",
+    name: "Uno",
+    image: "/images/uno.jpg",
+    description: "Classic card game where you match colors and numbers to be the first to get rid of your cards.",
+    category: "Card Game",
+    players: "2-10"
+  }
+];
+
+module.exports = games;
