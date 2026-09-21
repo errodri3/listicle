14756527a344ec1f813d239ceb82d92ca9bdc80e
Evelyn Rodriguez <errodriguez0703@gmail.com>
2026-09-20 23:16:14 -0700

    set up index.html and created script.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const games = require('./data/games');
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
