c3201897cde58820dba169c8afe1f5a0ccc520db
Evelyn Rodriguez <errodriguez0703@gmail.com>
2026-09-20 23:43:47 -0700

    add images to game list + edit game list

--- data/games.js
+++ data/games.js
@@ -2,7 +2,7 @@ const games = [
   {
     id: "magic-the-gathering",
     name: "Magic: The Gathering",
-    image: "/images/magic-the-gathering.jpg",
+    image: "/images/mtg.png",
     description: "Trading card game where players cast spells and summon creatures to defeat their opponent.",
     category: "Trading Card Game",
     players: "2+"
@@ -10,7 +10,7 @@ const games = [
   {
     id: "lorcana",
     name: "Disney Lorcana",
-    image: "/images/lorcana.jpg",
+    image: "/images/lorcana.png",
     description: "Trading card game where players use Disney characters as 'glimmers' to battle for control of the board.",
     category: "Trading Card Game",
     players: "2-4"
@@ -18,7 +18,7 @@ const games = [
   {
     id: "star-wars",
     name: "Star Wars: Unlimited",
-    image: "/images/star-wars.jpg",
+    image: "/images/starwars.png",
     description: "Trading card game set in the Star Wars universe where players build decks to defeat their opponent.",
     category: "Trading Card Game",
     players: "2"
@@ -26,7 +26,7 @@ const games = [
   {
     id: "riftbound",
     name: "Riftbound",
-    image: "/images/riftbound.jpg",
+    image: "/images/riftbound.png",
     description: "Trading card game based on the League of Legends universe.",
     category: "Trading Card Game",
     players: "2-4"
@@ -34,31 +34,31 @@ const games = [
   {
     id: "animal-crossing-monopoly",
     name: "Animal Crossing Monopoly",
-    image: "/images/animal-crossing-monopoly.jpg",
+    image: "/images/animal_crossing_monopoly.png",
     description: "Animal Crossing-themed version of Monopoly where you buy, sell, and trade to build up your island.",
     category: "Board Game",
     players: "2-6"
   },
   {
     id: "crabs-in-a-bucket",
-    name: "Crabs in a Bucket",
-    image: "/images/crabs-in-a-bucket.jpg",
-    description: "Party card game about pulling other players' cards to keep yourself in the bucket the longest.",
+    name: "Crabs in a Bucket: Shrimpocalypse",
+    image: "/images/crabs_in_a_bucket.png",
+    description: "Players try to stop opponents from shedding all their cards while simultaneously blocking anyone from executing the sudden Shrimpocalypse win.",
     category: "Party Game",
     players: "3-6"
   },
   {
-    id: "rainbow-shrimp",
-    name: "Rainbow Shrimp",
-    image: "/images/rainbow-shrimp.jpg",
-    description: "Fast-paced card matching game where players race to slap the matching card.",
+    id: "mantis",
+    name: "Mantis",
+    image: "/images/mantis.png",
+    description: "Fast-paced card game where you steal or score matching colored cards, first to 10 points wins.",
     category: "Party Game",
     players: "2-6"
   },
   {
     id: "uno",
     name: "Uno",
-    image: "/images/uno.jpg",
+    image: "/images/uno.png",
     description: "Classic card game where you match colors and numbers to be the first to get rid of your cards.",
     category: "Card Game",
     players: "2-10"
